8fbb804fc192183391dd0c8e12f841991d6bf7c4
x06lan <x065p4mp6@gmail.com>
2022-09-30 10:57:19 +0800

    fix error (static assert LENGHT->length

--- ut_length.h
+++ ut_length.h
@@ -1,27 +1,24 @@
-#include <gtest/gtest.h>
 #include "length.h"
-TEST(LENGTH, TwoDim)
-{
-    double arr[] = {3, 4};
-    int dim = 2;
-    double output = length(arr, dim);
-    ASSERT_EQ(output, 5);
+#include <gtest/gtest.h>
+TEST(length, TwoDim) {
+  double arr[] = {3, 4};
+  int dim = 2;
+  double output = length(arr, dim);
+  ASSERT_EQ(output, 5);
 }
 
-TEST(LENGTH, ThreeDim)
-{
-    double arr[] = {3, 4, 5};
-    int dim = 3;
-    double output = length(arr, dim);
-    ASSERT_NEAR(output, 7.07, 0.01);
+TEST(length, ThreeDim) {
+  double arr[] = {3, 4, 5};
+  int dim = 3;
+  double output = length(arr, dim);
+  ASSERT_NEAR(output, 7.07, 0.01);
 }
 
-TEST(LENGTH, TwoDimMathVector)
-{
-    double arr[] = {3, 4};
-    int dim = 2;
-    // MathVector m; -> default constructor
-    MathVector m(dim, arr);
-    double output = length(m);
-    ASSERT_EQ(output, 5);
+TEST(length, TwoDimMathVector) {
+  double arr[] = {3, 4};
+  int dim = 2;
+  // MathVector m; -> default constructor
+  MathVector m(dim, arr);
+  double output = length(m);
+  ASSERT_EQ(output, 5);
 }
